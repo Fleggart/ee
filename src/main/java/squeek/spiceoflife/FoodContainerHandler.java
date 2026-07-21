9a2000c56f8e51292e4e81641c1633e6865d1c70
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-22 06:01:37 +0900

    Update FoodContainerHandler.java

--- src/main/java/squeek/spiceoflife/FoodContainerHandler.java
+++ src/main/java/squeek/spiceoflife/FoodContainerHandler.java
@@ -1,17 +1,13 @@
 package squeek.spiceoflife;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import squeek.applecore.api.food.FoodEvent;
-import squeek.spiceoflife.foodtracker.FoodEaten;
-import squeek.spiceoflife.foodtracker.FoodHistory;
-import squeek.spiceoflife.foodtracker.FoodModifier;
-import squeek.spiceoflife.helpers.FoodHelper;
 
 public class FoodContainerHandler
 {
-	// 只保留必要的食物事件处理
-	// 如果不影响容器功能，可以完全清空
+    // 空事件处理器，仅为满足编译
+    @SubscribeEvent
+    public void onDummyEvent()
+    {
+        // 不需要做任何事情
+    }
 }
