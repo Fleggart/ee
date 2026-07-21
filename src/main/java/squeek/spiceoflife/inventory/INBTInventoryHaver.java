29e90e8224e45f40c0b8ac97bedc0f9703a66eb7
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-07-22 05:35:01 +0900

    Create INBTInventoryHaver.java

---
+++ src/main/java/squeek/spiceoflife/inventory/INBTInventoryHaver.java
@@ -0,0 +1,14 @@
+package squeek.spiceoflife.inventory;
+
+import net.minecraft.item.ItemStack;
+import javax.annotation.Nonnull;
+
+public interface INBTInventoryHaver
+{
+    int getSizeInventory();
+    String getInvName(NBTInventory inventory);
+    boolean hasCustomName(NBTInventory inventory);
+    int getInventoryStackLimit(NBTInventory inventory);
+    void onInventoryChanged(NBTInventory inventory);
+    boolean isItemValidForSlot(NBTInventory inventory, int slotNum, @Nonnull ItemStack itemStack);
+}
